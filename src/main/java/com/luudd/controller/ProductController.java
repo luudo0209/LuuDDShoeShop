bf7f7e27cb88418e2b3f3d30f6a36ea90df5920c
DucLuu <luudo0309@gmail.com>
2020-01-03 22:38:24 +0700

    Create ProductController.java - and Create createProduct method

--- src/main/java/com/luudd/controller/ProductController.java
+++ src/main/java/com/luudd/controller/ProductController.java
@@ -1,5 +1,25 @@
 package com.luudd.controller;
 
+import com.luudd.model.dto.ProductDTO;
+import com.luudd.model.request.ProductRequest;
+import com.luudd.service.serviceImpl.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/api/products")
+    public ResponseEntity<?> createUser(@RequestBody @Valid ProductRequest productRequest) {
+        ProductDTO product = productService.createProduct(productRequest);
+        return ResponseEntity.ok(product);
+    }
 }
