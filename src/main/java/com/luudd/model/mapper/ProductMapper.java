b107ccfbfa7165bfd3ceeffb1ea09160ffc97fc7
DucLuu <luudo0309@gmail.com>
2020-01-04 11:24:00 +0700

    Update ProductMapper.java

--- src/main/java/com/luudd/model/mapper/ProductMapper.java
+++ src/main/java/com/luudd/model/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package com.luudd.model.mapper;
 
 import com.luudd.entity.Product;
+import com.luudd.entity.ProductImage;
 import com.luudd.entity.User;
 import com.luudd.model.dto.ProductDTO;
 
@@ -8,12 +9,15 @@ import com.luudd.model.dto.ProductDTO;
  * Created by Do Duc Luu on 1/3/2020.
  */
 public class ProductMapper {
-    public ProductDTO toProductDTo(Product product){
+    public static ProductDTO toProductDTo(Product product){
         ProductDTO productDTO = new ProductDTO();
-        productDTO.setDescription(product.getDescription());
-        productDTO.setGenderFor(product.getGenderFor());
-        productDTO.setPrice(product.getPrice());
+        productDTO.setProductId(product.getProductId());
         productDTO.setProductName(product.getProductName());
+        productDTO.setSize(product.getSize());
+        productDTO.setDescription(product.getDescription());
+        productDTO.setCategory(product.getCategory());
+        productDTO.setGenderFor(product.getGenderFor());
+        productDTO.setProductImage(product.getProductImage());
         return productDTO;
     }
 }
