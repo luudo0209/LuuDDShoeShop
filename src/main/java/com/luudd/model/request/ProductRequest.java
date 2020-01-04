a4e6de91bb3ad42c5fb7adddfd7927f5b830f4e5
DucLuu <luudo0309@gmail.com>
2020-01-04 11:25:06 +0700

    Update ProductRequest.java

--- src/main/java/com/luudd/model/request/ProductRequest.java
+++ src/main/java/com/luudd/model/request/ProductRequest.java
@@ -1,38 +1,29 @@
 package com.luudd.model.request;
 
+import com.luudd.entity.Category;
+import com.luudd.entity.ProductImage;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * Created by Do Duc Luu on 1/3/2020.
  */
 @Data
 public class ProductRequest {
-    @NotNull(message = "Category is required")
     @ApiModelProperty(
             example="Duc Luu",
             notes="Category cannot be empty",
             required=true
     )
-    private int categoryId;
-    @NotNull
-    @NotEmpty
-    private String productName;
-    @NotNull
-    private String description;
-    @NotNull
-    @NotEmpty
-    private int productImageId;
-    @NotNull
-    @NotEmpty
-    private float price;
-    @NotNull
-    @NotEmpty
-    private double size;
-    @NotNull
-    @NotEmpty
-    private String genderFor;
+    private @NotNull(message = "Category is required") Category category;
+    private @NotNull @NotEmpty String productName;
+    private @NotNull String description;
+    private @NotNull @NotEmpty String productImage;
+    private  @NotNull float price;
+    private @NotNull double size;
+    private @NotNull @NotEmpty String genderFor;
 }
