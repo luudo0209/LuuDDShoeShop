492b29372612eb4f11df3048cb037332b5b33bd1
DucLuu <luudo0309@gmail.com>
2020-01-07 11:47:31 +0700

    Create ICategoryRepository.java

--- src/main/java/com/luudd/repository/ICategoryRepository.java
+++ src/main/java/com/luudd/repository/ICategoryRepository.java
@@ -1,7 +1,17 @@
 package com.luudd.repository;
 
+import com.luudd.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by Do Duc Luu on 1/5/2020.
  */
-public class ICategoryRepository {
+@Repository
+public interface ICategoryRepository extends JpaRepository<Category, Integer> {
+    @Query(nativeQuery = true , value = "SELECT * FROM category WHERE category_id = :id_category")
+    Category getCategoryById(@Param("id_category") Long id);
+
 }
