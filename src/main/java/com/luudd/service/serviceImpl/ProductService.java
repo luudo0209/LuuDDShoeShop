68df5a9d975d592b280d89bada3c5a573c5c5028
DucLuu <luudo0309@gmail.com>
2020-01-07 11:48:43 +0700

    Update ProductService.java

--- src/main/java/com/luudd/service/serviceImpl/ProductService.java
+++ src/main/java/com/luudd/service/serviceImpl/ProductService.java
@@ -1,11 +1,13 @@
 package com.luudd.service.serviceImpl;
 
+import com.luudd.entity.Category;
 import com.luudd.entity.Product;
 import com.luudd.entity.ProductImage;
 import com.luudd.model.dto.ProductDTO;
 import com.luudd.model.mapper.ProductMapper;
 import com.luudd.model.mapper.UserMapper;
 import com.luudd.model.request.ProductRequest;
+import com.luudd.repository.ICategoryRepository;
 import com.luudd.repository.IProductRepository;
 import com.luudd.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,47 +27,61 @@ public class ProductService implements IProductService {
 
     @Autowired
     private IProductRepository productRepository;
+    @Autowired
+    private ICategoryRepository iCategoryRepository;
 
     @Transactional
     @Override
     public ProductDTO createProduct(ProductRequest productRequest) {
-        //ProductDTO productDTO = new ProductDTO();
         Product product = new Product();
         ProductImage images = new ProductImage();
         Date date = new Date();
-        //Create product
-        product.setProductName(productRequest.getProductName());
-        product.setPrice(productRequest.getPrice());
-        product.setGenderFor(productRequest.getGenderFor());
-        product.setDescription(productRequest.getDescription());
-        product.setProductImage(productRequest.getProductImage());
-        product.setSize(productRequest.getSize());
-        product.setCreatedAt(date);
-        product.setCreatedBy("Luudd");
-        product.setUpdatedAt(date);
-        product.setUpdatedBy("Luudd");
-        productRepository.save(product);
-        //Save images to imageTable
-        images.setImages(productRequest.getProductImage());
-        return ProductMapper.toProductDTo(product);
-    }
-
-    @Override
-    public ProductDTO updateProduct(ProductRequest productRequest, int id) {
-        List<Product> productList = productRepository.findAll();
-        Date date = new Date();
-        for (Product product : productList) {
-            if(id == product.getProductId()) {
+        try {
+            Category category = iCategoryRepository.getCategoryById(productRequest.getCategoryId());
+            if (category != null){
                 product.setProductName(productRequest.getProductName());
                 product.setPrice(productRequest.getPrice());
                 product.setGenderFor(productRequest.getGenderFor());
                 product.setDescription(productRequest.getDescription());
-                product.setProductImage(productRequest.getProductImage());
                 product.setSize(productRequest.getSize());
+                product.setCategory(category);
+                product.setCreatedAt(date);
+                product.setCreatedBy("Luudd");
                 product.setUpdatedAt(date);
                 product.setUpdatedBy("Luudd");
+                productRepository.save(product);
                 return ProductMapper.toProductDTo(product);
+            } else {
+                return null;
             }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public ProductDTO updateProduct(ProductRequest productRequest, int id) {
+        List<Product> productList = productRepository.findAll();
+        Date date = new Date();
+        try {
+            Category category = iCategoryRepository.getCategoryById(productRequest.getCategoryId());
+            for (Product product : productList) {
+                if(id == product.getProductId() && category != null) {
+                    product.setProductName(productRequest.getProductName());
+                    product.setPrice(productRequest.getPrice());
+                    product.setGenderFor(productRequest.getGenderFor());
+                    product.setDescription(productRequest.getDescription());
+                    product.setSize(productRequest.getSize());
+                    product.setUpdatedAt(date);
+                    product.setUpdatedBy("Luudd");
+                    return ProductMapper.toProductDTo(product);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
         return null;
     }
@@ -92,7 +108,6 @@ public class ProductService implements IProductService {
         }
         return null;
     }
-
     @Override
     public List<ProductDTO> getAllProducts() {
         List<Product> productList = productRepository.findAll();
