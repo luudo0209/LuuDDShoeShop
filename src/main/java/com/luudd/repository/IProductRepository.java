4b5212076f96f7df9140deeb7381e5bbc3cc62e7
DucLuu <luudo0309@gmail.com>
2020-01-08 09:58:01 +0700

    Update IProductRepository.java

--- src/main/java/com/luudd/repository/IProductRepository.java
+++ src/main/java/com/luudd/repository/IProductRepository.java
@@ -6,11 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by Do Duc Luu on 1/3/2020.
  */
 @Repository
 public interface IProductRepository  extends JpaRepository<Product, Integer>{
-    /*@Query(nativeQuery = true, value = "SELECT * FROM product WHERE product_name LIKE '%:productName%'")
-    Product findProductByName(@Param("product_name") String productName);*/
+    @Query(nativeQuery = true , value = "SELECT * FROM product WHERE product_id = :product_id")
+    Product getProductById(@Param("product_id") int id);
+
+    @Query(nativeQuery = true , value = "SELECT * FROM product")
+    List<Product> getAllProducts();
 }
